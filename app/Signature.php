12881bfda35706d7e7684c1fe639f891dcc87d0a
Ryan Bamba <ryan.bamba@metrodeal.com>
2018-10-04 17:56:27 +0800

    Resource

--- app/Signature.php
+++ app/Signature.php
@@ -7,40 +7,41 @@ use Illuminate\Database\Eloquent\Model;
 class Signature extends Model
 {
   /**
-* Field to be mass-assigned.
-*
-* @var array
-*/
-protected $fillable = ['name', 'email', 'body', 'flagged_at'];
-}
+  * Field to be mass-assigned.
+  *
+  * @var array
+  */
+  protected $fillable = ['name', 'email', 'body', 'flagged_at'];
 
-/**
- * Ignore flagged signatures.
- *
- * @param $query
- * @return mixed
- */
-public function scopeIgnoreFlagged($query)
-{
-    return $query->where('flagged_at', null);
-}
 
-/**
- * Flag the given signature.
- *
- * @return bool
- */
-public function flag()
-{
-    return $this->update(['flagged_at' => \Carbon\Carbon::now()]);
-}
+  /**
+   * Ignore flagged signatures.
+   *
+   * @param $query
+   * @return mixed
+   */
+  public function scopeIgnoreFlagged($query)
+  {
+      return $query->where('flagged_at', null);
+  }
 
-/**
- * Get the user Gravatar by their email address.
- *
- * @return string
- */
-public function getAvatarAttribute()
-{
-    return sprintf('https://www.gravatar.com/avatar/%s?s=100', md5($this->email));
+  /**
+   * Flag the given signature.
+   *
+   * @return bool
+   */
+  public function flag()
+  {
+      return $this->update(['flagged_at' => \Carbon\Carbon::now()]);
+  }
+
+  /**
+   * Get the user Gravatar by their email address.
+   *
+   * @return string
+   */
+  public function getAvatarAttribute()
+  {
+      return sprintf('https://www.gravatar.com/avatar/%s?s=100', md5($this->email));
+  }
 }
