d5fb83844a956c327c37f66c2b40ca452907d0a7
Ryan Bamba <ryan.bamba@metrodeal.com>
2018-10-10 08:55:51 +0800

    finishing touches

--- resources/js/app.js
+++ resources/js/app.js
@@ -17,6 +17,7 @@ window.Vue = require('vue');
 
  Vue.component('signatures', require('./components/Signatures.vue'));
  Vue.component('signature-form', require('./components/SignatureForm.vue'));
+ Vue.component('paginate', require('vuejs-paginate'));
 
  const app = new Vue({
      el: '#app'
